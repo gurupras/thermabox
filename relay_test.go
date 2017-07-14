59e1630fa15cf6b91a54f0c000368a2d403cc599
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2017-07-14 09:14:32 -0400

    Added relay toggle test

--- relay_test.go
+++ relay_test.go
@@ -2,6 +2,7 @@ package thermabox
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -12,5 +13,28 @@ func TestRelay(t *testing.T) {
 	relay, err := NewRelay(true, []int{23, 22})
 	require.Nil(err)
 	require.NotNil(relay)
+
+}
+
+func TestRelayToggle(t *testing.T) {
+	require := require.New(t)
+
+	relay, err := NewRelay(false, []int{23, 22})
+	require.Nil(err)
+	require.NotNil(relay)
+
+	// Test switch 1
+	err = relay.Toggle(1)
+	require.Nil(err)
+	time.Sleep(500 * time.Second)
+	err = relay.Toggle(1)
+	require.Nil(err)
+
+	// Test switch 2
+	err = relay.Toggle(2)
+	require.Nil(err)
+	time.Sleep(500 * time.Second)
+	err = relay.Toggle(2)
+	require.Nil(err)
 
 }
