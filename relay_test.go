d24dcb7942bfab6e18dffd4da04bb1e2fd4c8844
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2017-07-14 09:20:19 -0400

    Added relay on/off test

--- relay_test.go
+++ relay_test.go
@@ -13,7 +13,6 @@ func TestRelay(t *testing.T) {
 	relay, err := NewRelay(true, []int{23, 22})
 	require.Nil(err)
 	require.NotNil(relay)
-
 }
 
 func TestRelayToggle(t *testing.T) {
@@ -37,5 +36,27 @@ func TestRelayToggle(t *testing.T) {
 	time.Sleep(500 * time.Millisecond)
 	err = relay.Toggle(2)
 	require.Nil(err)
+}
 
+func TestRelayOnOff(t *testing.T) {
+	require := require.New(t)
+
+	relay, err := NewRelay(false, []int{23, 22})
+	require.Nil(err)
+	require.NotNil(relay)
+
+	// Test switch 1
+	err = relay.On(1)
+	require.Nil(err)
+	time.Sleep(500 * time.Millisecond)
+	err = relay.Off(1)
+	require.Nil(err)
+	time.Sleep(500 * time.Millisecond)
+
+	// Test switch 2
+	err = relay.On(2)
+	require.Nil(err)
+	time.Sleep(500 * time.Millisecond)
+	err = relay.Off(2)
+	require.Nil(err)
 }
