30a0062b3f8800a304729d4a6c31f244e2fe0289
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2017-07-14 09:16:29 -0400

    Fixed a bug in relay toggle test

--- relay_test.go
+++ relay_test.go
@@ -26,14 +26,14 @@ func TestRelayToggle(t *testing.T) {
 	// Test switch 1
 	err = relay.Toggle(1)
 	require.Nil(err)
-	time.Sleep(500 * time.Second)
+	time.Sleep(500 * time.Millisecond)
 	err = relay.Toggle(1)
 	require.Nil(err)
 
 	// Test switch 2
 	err = relay.Toggle(2)
 	require.Nil(err)
-	time.Sleep(500 * time.Second)
+	time.Sleep(500 * time.Millisecond)
 	err = relay.Toggle(2)
 	require.Nil(err)
 
