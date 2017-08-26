bce55e7c4b7d514b4af4692f76f50d35804d23f4
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2017-08-26 15:16:33 -0400

    probe_http: Added retry logic

--- probe_http.go
+++ probe_http.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/parnurzeal/gorequest"
 )
@@ -13,19 +14,31 @@ type HTTPProbe struct {
 }
 
 func (p *HTTPProbe) GetTemperature() (float64, error) {
-	resp, body, errs := gorequest.New().Get(p.Url).End()
-	if len(errs) > 0 {
-		return 0, fmt.Errorf("Failed to get temperature: %v", errs)
-	}
+	var err error
+	for i := 0; i < 5; i++ {
+		var bodyStr string
+		var temp float64
+		var _err error
+		resp, body, errs := gorequest.New().Get(p.Url).End()
+		if len(errs) > 0 {
+			err = fmt.Errorf("Failed to get temperature: %v", errs)
+			goto retry
+		}
 
-	if resp != nil && resp.StatusCode != 200 {
-		return 0, fmt.Errorf("Failed to get temperature: Received response code: %v", resp.StatusCode)
-	}
+		if resp != nil && resp.StatusCode != 200 {
+			err = fmt.Errorf("Failed to get temperature: Received response code: %v", resp.StatusCode)
+			goto retry
+		}
 
-	bodyStr := strings.TrimSpace(string(body))
-	temp, err := strconv.ParseFloat(bodyStr, 64)
-	if err != nil {
-		return 0, fmt.Errorf("Failed to get temperature: %v", err)
+		bodyStr = strings.TrimSpace(string(body))
+		temp, _err = strconv.ParseFloat(bodyStr, 64)
+		if _err != nil {
+			err = fmt.Errorf("Failed to get temperature: %v", _err)
+			goto retry
+		}
+		return temp, nil
+	retry:
+		time.Sleep(100 * time.Millisecond)
 	}
-	return temp, nil
+	return 0, err
 }
