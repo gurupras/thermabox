51fb2edb4313b997af31297d4c5d8eab0eafd540
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2018-09-11 16:27:07 -0400

    Working on publishing thermabox data

--- interfaces/thermabox.go
+++ interfaces/thermabox.go
@@ -1,11 +1,31 @@
 package interfaces
 
+type State string
+
+const (
+	HEATING_UP   State = "heating_up"
+	COOLING_DOWN State = "cooling_down"
+	STABLE       State = "stable"
+	UNKNOWN      State = "unknown"
+)
+
 type TemperatureSensorInterface interface {
 	GetTemperature() (float64, error)
 }
 
+type ThermaboxState struct {
+	Temperature float64 `json:"temperature"`
+	Timestamp   int64   `json:"timestamp"`
+	State       State   `json:"state"`
+}
+
+type ThermaboxListenerInterface interface {
+	RegisterChannel(chan *ThermaboxState)
+}
+
 type ThermaboxInterface interface {
 	TemperatureSensorInterface
+	ThermaboxListenerInterface
 	SetLimits(temperature float64, threshold float64)
 	GetLimits() (temperature float64, threshold float64)
 	GetState() string
